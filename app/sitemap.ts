dd946182ec8093479db0c24867f05b45e50faf68
ikbalkmtnr786-glitch <ikbalkmtnr786@gmail.com>
2026-06-30 16:23:58 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const baseUrl = "https://pixospark.com";
+const baseUrl = "https://pixospark.vercel.app";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
